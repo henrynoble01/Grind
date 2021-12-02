0311abab76467d562602480c0dc0c04ecd248083
henrynoble01 <henrynoble01@gmail.com>
2021-12-02 05:44:01 +0100

    feat: initial commit of project

---
+++ app/routes/index.tsx
@@ -0,0 +1,8 @@
+export default function TestIndex() {
+  return (
+    <div>
+      <p>here is a random joke</p>
+      <p>I was wondering why the feisbee was getting bigger then it hit me</p>
+    </div>
+  );
+}
